615a35bbc02f16e87299210fe0f15ec489de2d58
CoreAnujDixit <anujdixitdseu@gmail.com>
2023-10-23 10:39:14 +0530

    added 404 error

--- src/App.js
+++ src/App.js
@@ -21,10 +21,10 @@ function App() {
               <NavLink to="/">Home</NavLink>
             </li>
             <li>
-              <NavLink  to="/">About</NavLink>
+              <NavLink to="/">About</NavLink>
             </li>
             <li>
-              <NavLink  to="/">Contact</NavLink>
+              <NavLink to="/">Contact</NavLink>
             </li>
             <li className="bg-green-200 py-2 px-4 rounded-md ">
               <button>
@@ -40,11 +40,29 @@ function App() {
         </div>
       </div>
       <Routes className="">
-        <Route path="/" element={<div className="text-center justify-center	flex  items-center	h-screen text-2xl font-bold">Hello</div>} />
+        <Route
+          path="/"
+          element={
+            <div className="text-center justify-center	flex  items-center	h-screen text-2xl font-bold">
+              Hello
+            </div>
+          }
+        />
         <Route path="/login" element={<Login />} />
         <Route path="/signin" element={<Signup />} />
         <Route path="/Register" element={<Registered />} />
         <Route path="/logged" element={<Welcome />} />
+        <Route
+          path="/*"
+          element={
+            <div>
+              <img className="w-fit absolute justify-center items-center"
+                src="https://assets-v2.lottiefiles.com/a/3455ed68-1151-11ee-9772-5b4c76d6674b/xn6epX0wkV.gif"
+                alt=""
+              />
+            </div>
+          }
+        />
       </Routes>
     </div>
   );
